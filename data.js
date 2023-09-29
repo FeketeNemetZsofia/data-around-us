daf4e58f727c2aee35f0c1a66530e22ad34de97a
FeketeNemetZsofia <zsofia.nemet91@gmail.com>
2023-09-29 15:08:49 +0200

    tasks done until Display specific characters

---
+++ data.js
@@ -0,0 +1,14 @@
+let title = "The Neverending Story"
+console.log(title)
+let author = "Michael Ende"
+console.log(author)
+let year = 1979
+console.log(year)
+let isNewerThan2000 = false
+console.log(isNewerThan2000)
+
+let age = 2023 - year
+console.log(age)
+
+let characters = ["Bastian Balthazar Bux", "Atreyu", "Falkor", "Childlike Empress"]
+console.log(characters)
